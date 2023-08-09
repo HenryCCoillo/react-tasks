8a713fa1194f329cf343bd89f085bb41c22e2a1c
Henry Ccoillo <henryccoillo11@gmail.com>
2023-08-09 17:26:36 -0500

    Inicio React

---
+++ src/data/task.js
@@ -0,0 +1,22 @@
+export const tasks = [
+    {
+        id:0,
+        title:"mi primer Task",
+        descripcion:"Mi primer Task"
+    },
+    {
+        id:1,
+        title:"mi segunda Task",
+        descripcion:"Mi segunda Task"
+    },
+    {
+        id:2,
+        title:"mi Tercera Task",
+        descripcion:"Mi Tercera Task"
+    },
+    {
+        id:3,
+        title:"mi cuarta Task",
+        descripcion:"Mi cuarta Task"
+    }
+]
